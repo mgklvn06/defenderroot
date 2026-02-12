e7b0c9e644f34425fcdcc00cd47c7d867428abbf
mgklvn06 <gkmwangi420@gmail.com>
2026-02-12 21:25:44 +0300

    added some functionality

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/defenderroot/',
   plugins: [
     vue(),
     vueDevTools(),
